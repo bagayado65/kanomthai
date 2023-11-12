fbc77dd5d37efab79664fa067f507f91dcb74b20
thanchanok1501 <milklove155@gmail.com>
2023-11-12 15:39:34 +0700

    up BolgNews

---
+++ src/pages/BlogNews5.js
@@ -0,0 +1,93 @@
+import React from "react";
+import "./BlogNews.css";
+
+class BlogNews5 extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="imgBlog">
+          <div className="topic-b-new">
+            <h1 className="topic"> ข่าว</h1>
+          </div>
+        </div>
+
+        <div className="blog-grid-container ">
+          <div className="topicNew-later">
+            <h2>
+              <strong>“หวานไทย” ผู้สืบสานภูมิปัญญาไทย</strong>
+            </h2>
+            <p>ถือเป็นซีอีโอรุ่นใหม่ไฟแรงที่มีแพสชั่นหลากหลายด้าน</p>
+          </div>
+          <img
+            className="blog-box-shadow imgBlog-content"
+            src="https://static.thairath.co.th/media/dFQROr7oWzulq5Fa5Bt4UgDgkpS21pi9Ed8cqP6hcpGquN0WKOZe0we1rK8LFfEoVdE.webp"
+          />
+          <div>
+            <h3>
+              <strong>“ต๊อด-ปิติ ภิรมย์ภักดี”</strong>
+            </h3>
+
+            <p className="text-p-n">
+              Posted By :<span className="color-text"> mgronline.com </span>
+            </p>
+          </div>
+          <div className="text-blog-content">
+            <p>
+              โดยล่าสุดจับมือ “เชฟชุมพล แจ้งไพร” เชฟมือทองด้านอาหารไทย
+              เปิดตัวร้าน “หวานไทย” (Waan Thai) ในซอยทองหล่อ 9
+              เพื่อให้เป็นคาเฟ่ขนมไทย, ของว่าง และอาหารไทยโบราณรูปแบบใหม่
+              ที่สะท้อนความเป็นไทยอย่างสวยงาม ต่อยอดจากความสำเร็จของร้าน
+              “R-Haan” ที่คว้ารางวัลมิชลินสตาร์ 1 ดาว และ 2 ดาว มาต่อเนื่องทุกปี
+            </p>
+          </div>
+
+          <img
+            className="blog-box-shadow-line imgBlog-content"
+            src="https://static.thairath.co.th/media/PZnhTOtr5D3rd9oc9nP8CPfzssYZ6nj0j4rGeomIVaLZFv0.webp"
+          />
+          <div className="text-left">
+                <p>งานนี้ทั้งคู่ตั้งใจที่จะรังสรรค์ขนมไทย, ของว่าง และอาหารไทยโบราณ ให้ออกมาเป็นอาหารและขนมที่ผสมผสานด้วยภูมิปัญญาไทย โดยยังคงรสชาติความเป็นไทยแท้ มีความละเมียดละไม และความพิถีพิถันในทุกขั้นตอน เป็นอาหารไทยสไตล์ใหม่ ภายใต้แนวคิด
+                                    “Thai Cuisine Thai Dessert” โดยจะเน้นการคัดสรรวัตถุดิบท้องถิ่นดีที่สุดในแต่ละภูมิภาคจากทั่วประเทศ เมนูเด่นยกให้ “ข้าวผัดปลากุเลาเค็มไข่เป็ดซูวีร์” ใช้ปลากุเลาเค็มแท้ๆจากร้านป้าอ้วน ตากใบ จ.นราธิวาส ที่ได้รับการขนานนามเป็น
+                                    “ราชินีแห่งปลาเค็ม” นำมาคลุกเคล้ากับส่วนผสมคัดสรรอย่างประณีต เพื่อปรุงเป็นเมนูจานอร่อยเกินห้ามใจ
+                </p>
+
+          </div>
+        
+          <img
+            className="blog-box-shadow-line imgBlog-content"
+            src="https://static.thairath.co.th/media/Dtbezn3nNUxytg04aiNEWzOAZ0ZR4VNDAMjrhlKHSMR0X0.webp"
+          />
+          <div className="text-left">
+                <p>ในเมนูอาหารคาวยังมี “ไข่พะโล้สูตรคุณย่า” เมนูสุดคลาสสิกที่ใช้ไข่เป็ดอย่างดีมาทำพะโล้และเคี่ยวจนเหนียวหนึบ อีกจานแนะนำคือ “กุ้งแม่น้ำเผา” เสิร์ฟพร้อมหมี่กรอบทรงเครื่องและทอดมันปลากราย ไม่น่าพลาดยังรวมถึง “ไส้กรอก ปลาแนมชาววัง”,
+                                    “ขนมจีน น้ำพริกกุ้งสด”, “ข้าวตังหน้าตั้ง” และ “แกงเขียวหวาน เนื้อโคขุนโรตี”
+                                </p>
+          </div>
+         
+
+
+          <img
+            className="blog-box-shadow-line imgBlog-content"
+            src="https://static.thairath.co.th/media/Dtbezn3nNUxytg04aiNEWzOAZ0ZR4VNDOiVdAGifWovHvd.webp"
+          />
+
+            <div className="text-left">
+                <p>
+                                    ขนมไทยที่บรรจุอยู่ในเมนู มีอาทิ “ขนมตาล” ใช้เนื้อตาลสดๆ จาก เพชรบุรีได้ความนุ่ม หอมกลิ่นตาลแท้ๆ ส่วน “ขนมบ้าบิ่นมะพร้าวอ่อน” ใช้มะพร้าวน้ำหอมชั้นเลิศจากบ้านแพ้ว เสริมทัพด้วย “ทับทิมสยามหวานไทย” เสิร์ฟมาในลูกมะพร้าว, “ซูเฟล่น้ำกะทิแตงไทย”, “มิลล์ เฟยหม้อแกงเผือกหอม”,
+                                    “ขนมจ่ามงกุฎ” และ “ขนมครกไข่ปลาคาเวียร์”.
+                                    </p>
+            </div>
+         <img
+            className="blog-box-shadow-line imgBlog-content"
+            src="https://static.thairath.co.th/media/Dtbezn3nNUxytg04aiNEWzOAZ0ZR4VNCxQxjLsnvE1vIVK.webp"
+          />
+          <div className="text-left">
+            <h2><strong>ข้อมูลจาก</strong> : ไทยรัฐออนไลน์</h2>
+          </div>
+         
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BlogNews5;
--- src/pages/BulletinBoard.js
+++ src/pages/BulletinBoard.js
@@ -1,164 +1,179 @@
 import React from "react";
-import './BulletinBoardStyle.css';
+import "./BulletinBoardStyle.css";
+import { Link } from "react-router-dom";
 // import Button from 'react-bootstrap/Button';
 // import Card from 'react-bootstrap/Card';
 
+class BulletinBoard extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="imgBulletinBoard">
+          <div className="topic-b-new">
+            <h1 className="topic"> ข่าว</h1>
+          </div>
+        </div>
 
-class BulletinBoard extends React.Component{
-    render(){
-        return(
-            <div >
-                <div className="imgBulletinBoard">
-                    <div className="topic-b-new">
-                        <h1 className="topic"> ข่าว</h1>
-                       
-                    </div>
-                </div>
-                
-                <div className="topicNew-later">
-                    <h2>ข่าวใหม่ล่าสุด&nbsp;</h2>
-                    <p>เว็บไซด์แชร์ขนมไทยครบเครื่องเรื่องข่าวสารขนมไทย</p>
-                </div>
+        <div className="topicNew-later">
+          <h2>ข่าวใหม่ล่าสุด&nbsp;</h2>
+          <p>เว็บไซด์แชร์ขนมไทยครบเครื่องเรื่องข่าวสารขนมไทย</p>
+        </div>
 
-                
-                <div class="Bulletin-grid-container " >
-                    <div class="Bulletin-card">
-                       
-                            <img src='https://img.pptvhd36.com/health/thumbor/2022/12/15/cusArticle-05a1235.avif' class="Bulletin-image  "   />
-                       
-                            <div class="Bulletin-content ">
-                                <a href="#">
-                                <span class="Bulletin-title ">
-                                    ปริมาณน้ำตาลใน "ขนมไทย" กินได้แต่พอดี ลดเสี่ยงก่อโรคเบาหวาน
-                                </span>
-                                </a>
+        <div class="Bulletin-grid-container ">
+          <div class="Bulletin-card">
+            <img
+              src="https://img.pptvhd36.com/health/thumbor/2022/12/15/cusArticle-05a1235.avif"
+              class="Bulletin-image  "
+            />
 
-                                <p class="desc">
-                                ขนมไทยในงานมงคลต่างๆ โดยเฉพาะใกล้เทศกาลปีใหม่ มักมีขนมนมเนยและขนมไทยร่วมในลิสต์เฉลิมฉลอง เพราะรสชาติหวานที่ถูกใจ และคุ้ยเคย 
-                                </p>
+            <div class="Bulletin-content ">
+              <a href="#">
+                <span class="Bulletin-title ">
+                  ปริมาณน้ำตาลใน "ขนมไทย" กินได้แต่พอดี ลดเสี่ยงก่อโรคเบาหวาน
+                </span>
+              </a>
 
-                                <button class=' Bulletin-button ' >
-                                    อ่านเพิ่มเติม
-                                </button>
+              <p class="desc">
+                ขนมไทยในงานมงคลต่างๆ โดยเฉพาะใกล้เทศกาลปีใหม่
+                มักมีขนมนมเนยและขนมไทยร่วมในลิสต์เฉลิมฉลอง
+                เพราะรสชาติหวานที่ถูกใจ และคุ้ยเคย
+              </p>
 
-                            </div>
-                    </div>
-                    <div class="Bulletin-card">
-                       
-                            <img src='https://static.cordonbleu.edu/Files/MediaFile/77184.jpg' class="Bulletin-image  "   />
-                       
-                            <div class="Bulletin-content ">
-                                <a href="#">
-                                <span class="Bulletin-title ">
-                                    เชฟบีม ภวินวัชร์ ท็อปเชฟไทยแลนด์ ขนมหวานคนแรกของประเทศไทย
-                                </span>
-                                </a>
+              <Link to="/BlogNews">
+                <button class=" Bulletin-button "> อ่านเพิ่มเติม </button>
+              </Link>
+            </div>
+          </div>
+          <div class="Bulletin-card">
+            <img
+              src="https://static.cordonbleu.edu/Files/MediaFile/77184.jpg"
+              class="Bulletin-image  "
+            />
 
-                                <p class="desc">
-                                    เชฟบีม - ภวินวัชร์ โชคเศรษฐปวินท์ ศิษย์เก่าหลักสูตรการประกอบขนมอบชั้นสูงจากเลอ กอร์ดอง เบลอ ดุสิต Top Chef Thailandขนมหวาน คนแรกของประเทศไทย
-                                </p>
+            <div class="Bulletin-content ">
+              <a href="#">
+                <span class="Bulletin-title ">
+                  เชฟบีม ภวินวัชร์ ท็อปเชฟไทยแลนด์ ขนมหวานคนแรกของประเทศไทย
+                </span>
+              </a>
 
-                                <button class=' Bulletin-button ' >
-                                    อ่านเพิ่มเติม
-                                </button>
+              <p class="desc">
+                เชฟบีม - ภวินวัชร์ โชคเศรษฐปวินท์
+                ศิษย์เก่าหลักสูตรการประกอบขนมอบชั้นสูงจากเลอ กอร์ดอง เบลอ ดุสิต
+                Top Chef Thailandขนมหวาน คนแรกของประเทศไทย
+              </p>
 
-                            </div>
-                    </div>
-                    <div class="Bulletin-card">
-                       
-                            <img src='https://media.komchadluek.net/uploads/images/contents/w1024/2022/08/8h1cVt9sCpmlQkumfRAp.webp' class="Bulletin-image  "   />
-                       
-                            <div class="Bulletin-content ">
-                                <a href="#">
-                                <span class="Bulletin-title ">
-                                    ลูกชุบ" ขนมไทย ทำไมถึงดังไกล ครองใจผู้บริโภคชาวจีน ทำขายราคาดี
-                                </span>
-                                </a>
+              <Link to="/BlogNews2">
+                <button class=" Bulletin-button "> อ่านเพิ่มเติม </button>
+              </Link>
+            </div>
+          </div>
+          <div class="Bulletin-card">
+            <img
+              src="https://media.komchadluek.net/uploads/images/contents/w1024/2022/08/8h1cVt9sCpmlQkumfRAp.webp"
+              class="Bulletin-image  "
+            />
 
-                                <p class="desc">
-                                    ครจะรู้ว่าขนมไทย ที่มีความโดดเด่นด้วยรูปลักษณ์ มีสีสันที่สวยงาม หน้าตาน่ารับประทานสุด ๆ และมีรสชาติหวานหอมอร่อยมากเช่นกัน อย่างขนม "ลูกชุบ" 
-                                </p>
+            <div class="Bulletin-content ">
+              <a href="#">
+                <span class="Bulletin-title ">
+                  ลูกชุบ" ขนมไทย ทำไมถึงดังไกล ครองใจผู้บริโภคชาวจีน ทำขายราคาดี
+                </span>
+              </a>
 
-                                <button class=' Bulletin-button ' >
-                                    อ่านเพิ่มเติม
-                                </button>
+              <p class="desc">
+                ครจะรู้ว่าขนมไทย ที่มีความโดดเด่นด้วยรูปลักษณ์ มีสีสันที่สวยงาม
+                หน้าตาน่ารับประทานสุด ๆ และมีรสชาติหวานหอมอร่อยมากเช่นกัน
+                อย่างขนม "ลูกชุบ"
+              </p>
 
-                            </div>
-                    </div>
-                    <div class="Bulletin-card">
-                       
-                            <img src='https://static.thairath.co.th/media/Dtbezn3nNUxytg04aiHunbJ3Ckv9avYOwdKFKrWfZ73BOF.webp' class="Bulletin-image  "   />
-                       
-                            <div class="Bulletin-content ">
-                                <a href="#">
-                                <span class="Bulletin-title ">
-                                    สุดยอดนักปั้นบัวลอย มือวางอันดับหนึ่งเมืองโอ่ง เร็วสุดวินาทีละ 2 เม็ด
-                                </span>
-                                </a>
+              <Link to="/BlogNews3">
+                <button class=" Bulletin-button "> อ่านเพิ่มเติม </button>
+              </Link>
 
-                                <p class="desc">
-                                    ที่ร้านบัวลอยอรวรรณ บริเวณหน้าวัดโรงช้าง อ.เมือง จ.ราชบุรี มีสุดยอดนักปั้นบัวลอยเร็ววินาทีละ 2 เม็ด แถมรสชาติอร่อย จนมีลูกค้าติดอกติดใจ มาอุดหนุนกันเป็นจำนวนมากm 
-                                </p>
+             
+             
+            </div>
+          </div>
+          <div class="Bulletin-card">
+            <img
+              src="https://static.thairath.co.th/media/Dtbezn3nNUxytg04aiHunbJ3Ckv9avYOwdKFKrWfZ73BOF.webp"
+              class="Bulletin-image  "
+            />
 
-                                <button class=' Bulletin-button ' >
-                                    อ่านเพิ่มเติม
-                                </button>
+            <div class="Bulletin-content ">
+              <a href="#">
+                <span class="Bulletin-title ">
+                  สุดยอดนักปั้นบัวลอย มือวางอันดับหนึ่งเมืองโอ่ง เร็วสุดวินาทีละ
+                  2 เม็ด
+                </span>
+              </a>
 
-                            </div>
-                    </div>
-                    <div class="Bulletin-card">
-                       
-                            <img src='https://image.bangkokbiznews.com/uploads/images/md/2022/10/xOhyh3Rj0dLW8Z4t5Und.webp?x-image-process=style/LG-webp' class="Bulletin-image  "   />
-                       
-                            <div class="Bulletin-content ">
-                                <a href="#">
-                                <span class="Bulletin-title ">
-                                    ร้านขนมไทย “หวานไทย” ผู้สืบสานภูมิปัญญาไทย
-                                </span>
-                                </a>
+              <p class="desc">
+                ที่ร้านบัวลอยอรวรรณ บริเวณหน้าวัดโรงช้าง อ.เมือง จ.ราชบุรี
+                มีสุดยอดนักปั้นบัวลอยเร็ววินาทีละ 2 เม็ด แถมรสชาติอร่อย
+                จนมีลูกค้าติดอกติดใจ มาอุดหนุนกันเป็นจำนวนมากm
+              </p>
 
-                                <p class="desc">
-                                    ร้านขนมไทย หวานไทย (Waan Thai) โดย เชฟชุมพล แจ้งไพร เปิดใหม่ติดกับร้าน อาหาร (R-HAAN) เพื่อต่อยอด อาหารไทย และ ขนมไทย ในดีไซน์สวย ฯ 
-                                </p>
+              <Link to="/BlogNews4">
+                <button class=" Bulletin-button "> อ่านเพิ่มเติม </button>
+              </Link>
+             
+            </div>
+          </div>
+          <div class="Bulletin-card">
+            <img
+              src="https://image.bangkokbiznews.com/uploads/images/md/2022/10/xOhyh3Rj0dLW8Z4t5Und.webp?x-image-process=style/LG-webp"
+              class="Bulletin-image  "
+            />
 
-                                <button class=' Bulletin-button ' >
-                                    อ่านเพิ่มเติม
-                                </button>
+            <div class="Bulletin-content ">
+              <a href="#">
+                <span class="Bulletin-title ">
+                  ร้านขนมไทย “หวานไทย” ผู้สืบสานภูมิปัญญาไทย
+                </span>
+              </a>
 
-                            </div>
-                    </div>
-                    <div class="Bulletin-card">
-                       
-                            <img src='https://static.thairath.co.th/media/dFQROr7oWzulq5Fa5BYc4mGUmcA0Hf2QtYIgKPrZG9coNQyDuSfzclfp2W9dWlqJtzj.webp' class="Bulletin-image  "   />
-                       
-                            <div class="Bulletin-content ">
-                                <a href="#">
-                                <span class="Bulletin-title ">
-                                    ขนมใส่ไส้” สูตรไม่ธรรมดา ขายดิบขายดีมากว่า 10 ปี
-                                </span>
-                                </a>
+              <p class="desc">
+                ร้านขนมไทย หวานไทย (Waan Thai) โดย เชฟชุมพล แจ้งไพร
+                เปิดใหม่ติดกับร้าน อาหาร (R-HAAN) เพื่อต่อยอด อาหารไทย และ
+                ขนมไทย ในดีไซน์สวย ฯ
+              </p>
 
-                                <p class="desc">
-                                ขนมใส่ไส้ ตรงข้ามโรงเรียนประชาราษฎร์อุปถัมภ์วิทยา ใกล้สี่แยกลำกะโหลก ถนนพระยาสุเรนทร์ แขวงบางชัน กทม. ยืนหนึ่งเรื่องความอร่อยมานานนับ 10 ปีเต็มแล้ว
-                                </p>
+              <Link to="/BlogNews5">
+                <button class=" Bulletin-button "> อ่านเพิ่มเติม </button>
+              </Link>
+       
+            </div>
+          </div>
+          <div class="Bulletin-card">
+            <img
+              src="https://static.thairath.co.th/media/dFQROr7oWzulq5Fa5BYc4mGUmcA0Hf2QtYIgKPrZG9coNQyDuSfzclfp2W9dWlqJtzj.webp"
+              class="Bulletin-image  "
+            />
 
-                                <button class=' Bulletin-button ' >
-                                    อ่านเพิ่มเติม
-                                </button>
+            <div class="Bulletin-content ">
+              <a href="#">
+                <span class="Bulletin-title ">
+                  ขนมใส่ไส้” สูตรไม่ธรรมดา ขายดิบขายดีมากว่า 10 ปี
+                </span>
+              </a>
 
-                            </div>
-                    </div>
-                        
-                     
-                </div>
+              <p class="desc">
+                ขนมใส่ไส้ ตรงข้ามโรงเรียนประชาราษฎร์อุปถัมภ์วิทยา
+                ใกล้สี่แยกลำกะโหลก ถนนพระยาสุเรนทร์ แขวงบางชัน กทม.
+                ยืนหนึ่งเรื่องความอร่อยมานานนับ 10 ปีเต็มแล้ว
+              </p>
 
+              <Link to="/BlogNews6">
+                <button class=" Bulletin-button "> อ่านเพิ่มเติม </button>
+              </Link>
               
+            </div>
+          </div>
+        </div>
 
-                
-                   
-                                        
-  					
-    				{/* <div class="blog-detail">
+        {/* <div class="blog-detail">
                             <img src="public\images\stuff-img-01.jpg"/>
 							<h4>เชฟบีม ภวินวัชร์ ท็อปเชฟไทยแลนด์ ขนมหวานคนแรกของประเทศไทย</h4>
 
@@ -167,21 +182,9 @@ class BulletinBoard extends React.Component{
 							<a>เชฟบีม - ภวินวัชร์ โชคเศรษฐปวินท์ ศิษย์เก่าหลักสูตรการประกอบขนมอบชั้นสูงจากเลอ กอร์ดอง เบลอ ดุสิต Top Chef Thailandขนมหวาน คนแรกของประเทศไทย</a>
 							<a class="btn btn-lg btn-circle btn-outline-new-white" href="blog-details1.html">อ่านเพิ่มเติม</a>
 						</div> */}
-					
-				
-                
-
-              
-             
-                
-
-            </div>
-
-         
-        )
-        
-
-    }
+      </div>
+    );
+  }
 }
 
 export default BulletinBoard;
